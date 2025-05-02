82f6ca0a4764c03c454493710b6d541f5c0d15da
Refael-Pangaribuan17 <refaelpangaribuan123@gmail.com>
2025-05-02 10:21:27 +0700

    .

--- README.md
+++
@@ -1,8 +0,0 @@
-<img src="https://media.giphy.com/media/GRPy8MKag9U1U88hzY/giphy.gif" width="800"/>
-
-# 👋 Hi, I'm Refael Pangaribuan
-
-Saya mahasiswa tingkat akhir D3 Teknologi Komputer di Institut Teknologi Del.
-lihat portfolio saya di bawah
-
-https://preview--refael-pangaribuan.lovable.app/
--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all components for easy importing
-export { Navbar } from "./Navbar";
-export { HeroSection } from "./HeroSection";
-export { AboutSection } from "./AboutSection";
-export { ProjectsSection } from "./ProjectsSection";
-export { ContactSection } from "./ContactSection";
-export { Footer } from "./Footer";
-export { ThemeToggle } from "./ThemeToggle";
-export { PrestasiAkademikSection } from "./PrestasiAkademikSection";
